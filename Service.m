dd644302b299d67870bf3527d085cab1a378858e
xingwang.wxw <xingwang.wxw@alibaba-inc.com>
2017-03-14 17:02:28 +0800

    改名Service。

---
+++ Service.m
@@ -0,0 +1,46 @@
+//
+//  service.m
+//  PluginsDemo
+//
+//  Created by alisports on 2017/3/4.
+//  Copyright © 2017年 alisports. All rights reserved.
+//
+
+#import "Service.h"
+
+@implementation Service
+
+- (instancetype)init:(ServiceType)serviceType serviceName:(NSString *)serviceName serviceAction:(ServiceAction)serviceAction{
+    if (self = [super init]) {
+        _serviceType = serviceType;
+        _serviceName = serviceName;
+        _serviceAction = serviceAction;
+    }
+    return self;
+}
+
++ (ServiceType)convertServiceTypeFromString:(NSString *)yy{
+    if ([yy isEqualToString:@"http"] || [yy isEqualToString:@"https"]) {
+        return HTTP;
+    }
+    return HTTP;
+}
++ (ServiceAction)convertServiceActionFromString:(NSString *)yy{
+    if ([yy isEqualToString:@"resume"]) {
+        return Resume;
+    }else if ([yy isEqualToString:@"cancel"]) {
+        return Cancel;
+    }
+    
+    return Resume;
+}
+
+
+- (instancetype)copyWithZone:(NSZone *)zone{
+    Service *copy = [[[self class] allocWithZone:zone] init:_serviceType serviceName:_serviceName serviceAction:_serviceAction];
+    return copy;
+
+}
+
+
+@end
